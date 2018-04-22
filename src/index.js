e2bbca1f529f0a88985968455c98020c9b329581
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-04-22 13:40:13 -0500

    Adding SemanticUI

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './semantic/dist/semantic.min.css';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
